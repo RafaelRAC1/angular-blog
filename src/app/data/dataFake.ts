706fa38a367642a58efb14d22f3e217cf8cddc38
RafaelRAC1 <rafaelcorrea2154r@gmail.com>
2023-10-21 17:12:37 -0300

    content creation

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":1,
+        "photo":"https://s2-autoesporte.glbimg.com/8AsyIQCc2_sNbOsJXNUPQI87qYw=/0x0:1920x1080/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2022/U/M/0AQABNTsW6kmItO0mGaQ/sem-titulo.jpg",
+        "title": "WHY ARE HONDAS SO GOOD?",
+        "description": "There must be a explation to why Honda cars are so good. Let's walk through this article and discover how Honda develop reliable cars"
+    },
+    {
+        "id":2,
+        "photo":"https://s2-autoesporte.glbimg.com/hg5udYG8b6tPWiE0n3Vts8S_ZGw=/0x0:620x400/600x0/smart/filters:gifv():strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/S/D/o4Xa9GR1qpotOS06HFvg/2015-09-24-dianteira.jpg",
+        "title": "Honda or Toyota? Subaru!",
+        "description": "I love Subarus! :)"
+    }
+]
